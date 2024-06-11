e002fddc4b4d5e9db53e1aa80481f14acc5023bd
John Doe <johndoe@example.com>
2024-06-11 14:48:52 +0200

    finish lab

--- index.js
+++ index.js
@@ -8,9 +8,29 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister =
+  s1 +
+  " " +
+  s2 +
+  " " +
+  s3 +
+  " " +
+  s4 +
+  " " +
+  s5 +
+  " " +
+  s3 +
+  " " +
+  s2 +
+  " " +
+  s1 +
+  " " +
+  s4;
 
 // Print out the concatenated string
+console.log(tongueTwister);
+const part1 = "java";
+const part2 = "script";
 
 
 
@@ -18,15 +38,19 @@ const s5 = "and";
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
-const part1 = "java";
-const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const camelTail =
+  part1.slice(0, part1.length - 1) +
+  part1.charAt(part1.length - 1).toUpperCase() +
+  part2.slice(0, part2.length - 1) +
+  part2.charAt(part2.length - 1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
 
 
+console.log(camelTail);
 
 
 /*******************************************
@@ -36,10 +60,12 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
 
 
+console.log(tipAmount);
 
 
 /*******************************************
@@ -48,9 +74,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
 
+console.log(randomNumber);
 
 
 /*******************************************
